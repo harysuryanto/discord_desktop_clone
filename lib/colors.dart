2b8f039eeed80e877a724aff4a148dbe57ce784c
Hary Suryanto <hary.suryanto01@gmail.com>
2022-05-10 01:27:27 +0700

    add new colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,3 +4,5 @@ const grey1 = Color.fromRGBO(32, 34, 37, 1);
 const grey2 = Color.fromRGBO(41, 43, 47, 1);
 const grey3 = Color.fromRGBO(47, 49, 54, 1);
 const grey4 = Color.fromRGBO(54, 57, 63, 1);
+const green = Color.fromRGBO(59, 165, 93, 1);
+const blueAccent = Color.fromRGBO(88, 101, 242, 1);
