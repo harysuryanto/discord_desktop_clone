d01127114965fa8b5c890cb4dd50eab6af1f5515
Hary Suryanto <hary.suryanto01@gmail.com>
2022-05-11 23:19:56 +0700

    create new widgets

---
+++ lib/widgets/avatar_with_online_status.dart
@@ -0,0 +1,48 @@
+import 'package:discord_desktop_clone/colors.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+
+class AvatarWithOnlineStatus extends StatelessWidget {
+  const AvatarWithOnlineStatus({
+    Key? key,
+    this.imageUrl,
+    required this.username,
+    this.isOnline = false,
+  }) : super(key: key);
+
+  final String? imageUrl;
+  final String username;
+  final bool isOnline;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: 32,
+          height: 32,
+          decoration: BoxDecoration(
+            color: grey4,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: imageUrl == null
+              ? Center(child: Text(username.substring(0, 1)))
+              : Image.network(imageUrl!),
+        ),
+        if (isOnline)
+          const Positioned(
+            bottom: -3,
+            right: -3,
+            child: CircleAvatar(
+              radius: 7,
+              backgroundColor: grey2,
+              child: CircleAvatar(
+                radius: 4,
+                backgroundColor: green,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/online_list_tile.dart
@@ -0,0 +1,42 @@
+import 'package:discord_desktop_clone/widgets/avatar_with_online_status.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+
+class OnlineListTile extends StatelessWidget {
+  const OnlineListTile({
+    Key? key,
+    this.imageUrl,
+    required this.username,
+    this.activity,
+    this.isOnline = true,
+  }) : super(key: key);
+
+  final String? imageUrl;
+  final String username;
+  final String? activity;
+  final bool isOnline;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: isOnline ? 1 : 0.3,
+      child: Row(
+        children: [
+          AvatarWithOnlineStatus(
+            username: username,
+            imageUrl: imageUrl,
+            isOnline: isOnline,
+          ),
+          const SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(username),
+              if (activity != null)
+                Text(activity!, style: const TextStyle(fontSize: 11)),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
