ab4a3eecc6ff4aaf113ebf6e2f982fde23886187
Hary Suryanto <hary.suryanto01@gmail.com>
2022-05-12 00:25:59 +0700

    extract a widget

--- lib/widgets/server/server.dart
+++ lib/widgets/server/server.dart
@@ -1,4 +1,5 @@
-import '../../colors.dart';
+import 'server_list_tile.dart';
+
 import 'package:fluent_ui/fluent_ui.dart';
 
 class Server extends StatelessWidget {
@@ -25,62 +26,6 @@ class Server extends StatelessWidget {
               imageUrl:
                   'https://cdn.discordapp.com/icons/757200769819869226/f3cf101df6b34c91997101438e56c072.webp?size=96'),
         ],
-      ),
-    );
-  }
-}
-
-class ServerListTile extends StatefulWidget {
-  const ServerListTile({
-    Key? key,
-    required this.serverName,
-    this.imageUrl,
-  }) : super(key: key);
-
-  final String serverName;
-  final String? imageUrl;
-
-  @override
-  State<ServerListTile> createState() => _ServerListTileState();
-}
-
-class _ServerListTileState extends State<ServerListTile> {
-  bool isHovering = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return MouseRegion(
-      onHover: (event) => setState(() => isHovering = true),
-      onExit: (event) => setState(() => isHovering = false),
-      cursor: SystemMouseCursors.click,
-      child: _buildRoundedRectangle(
-        isHovering: isHovering,
-        child: widget.imageUrl == null
-            ? Text(widget.serverName.substring(0, 1))
-            : Image.network(widget.imageUrl!),
-      ),
-    );
-  }
-
-  Widget _buildRoundedRectangle({
-    required bool isHovering,
-    required Widget child,
-  }) {
-    final BorderRadius unhoveredBorderRadius = BorderRadius.circular(50);
-    final BorderRadius hoveredBorderRadius = BorderRadius.circular(15);
-
-    return AspectRatio(
-      aspectRatio: 1,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        decoration: BoxDecoration(
-          color: isHovering ? blueAccent : grey4,
-          borderRadius:
-              isHovering ? hoveredBorderRadius : unhoveredBorderRadius,
-        ),
-        alignment: Alignment.center,
-        clipBehavior: Clip.antiAlias,
-        child: child,
       ),
     );
   }
---
+++ lib/widgets/server/server_list_tile.dart
@@ -0,0 +1,59 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+import '../../colors.dart';
+
+class ServerListTile extends StatefulWidget {
+  const ServerListTile({
+    Key? key,
+    required this.serverName,
+    this.imageUrl,
+  }) : super(key: key);
+
+  final String serverName;
+  final String? imageUrl;
+
+  @override
+  State<ServerListTile> createState() => _ServerListTileState();
+}
+
+class _ServerListTileState extends State<ServerListTile> {
+  bool isHovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onHover: (event) => setState(() => isHovering = true),
+      onExit: (event) => setState(() => isHovering = false),
+      cursor: SystemMouseCursors.click,
+      child: _buildRoundedRectangle(
+        isHovering: isHovering,
+        child: widget.imageUrl == null
+            ? Text(widget.serverName.substring(0, 1))
+            : Image.network(widget.imageUrl!),
+      ),
+    );
+  }
+
+  Widget _buildRoundedRectangle({
+    required bool isHovering,
+    required Widget child,
+  }) {
+    final BorderRadius unhoveredBorderRadius = BorderRadius.circular(50);
+    final BorderRadius hoveredBorderRadius = BorderRadius.circular(15);
+
+    return AspectRatio(
+      aspectRatio: 1,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        decoration: BoxDecoration(
+          color: isHovering ? blueAccent : grey4,
+          borderRadius:
+              isHovering ? hoveredBorderRadius : unhoveredBorderRadius,
+        ),
+        alignment: Alignment.center,
+        clipBehavior: Clip.antiAlias,
+        child: child,
+      ),
+    );
+  }
+}
