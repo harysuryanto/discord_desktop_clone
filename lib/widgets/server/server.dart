ed4ccfb65129305aeb3bf6d2be774a8c283de1c9
Hary Suryanto <hary.suryanto01@gmail.com>
2022-05-20 02:24:44 +0700

    improve ui and add data

--- lib/widgets/server/server.dart
+++ lib/widgets/server/server.dart
@@ -7,22 +7,28 @@ class Server extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 65,
+    return LimitedBox(
+      maxWidth: 72,
       child: ListView(
         controller: ScrollController(),
-        padding: const EdgeInsets.all(12),
         children: const [
+          SizedBox(height: 12),
           ServerListTile(serverName: 'Home'),
           SizedBox(height: 10),
           Divider(),
           SizedBox(height: 10),
-          ServerListTile(serverName: 'Pisang'),
-          SizedBox(height: 10),
           ServerListTile(
               serverName: 'RPL-B Squad',
               imageUrl:
-                  'https://cdn.discordapp.com/icons/757200769819869226/f3cf101df6b34c91997101438e56c072.webp?size=96'),
+                  'https://cdn.discordapp.com/icons/757200769819869226/f3cf101df6b34c91997101438e56c072.webp?size=96',
+              isActive: true),
+          SizedBox(height: 10),
+          ServerListTile(
+              serverName: 'DSE-C Prak. Mobile',
+              imageUrl:
+                  'https://cdn.discordapp.com/icons/690454124809945130/03640d6c24bbc25be422c3d4b56306f9.webp?size=80'),
+          SizedBox(height: 10),
+          ServerListTile(serverName: 'Pisang'),
           SizedBox(height: 10),
           ServerListTile(
             serverName: 'Add',
@@ -35,7 +41,7 @@ class Server extends StatelessWidget {
             isAnActionButton: true,
             tooltipMessage: 'Explore Public Servers',
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 12),
         ],
       ),
     );
