589696793f8f7d39a7006626746943e9c957a9c7
Hary Suryanto <hary.suryanto01@gmail.com>
2022-05-21 01:11:52 +0700

    improve ui

--- lib/widgets/global/avatar_with_online_status.dart
+++ lib/widgets/global/avatar_with_online_status.dart
@@ -8,27 +8,33 @@ class AvatarWithOnlineStatus extends StatelessWidget {
     this.imageUrl,
     required this.username,
     this.isOnline = false,
+    this.width = 32,
+    this.height = 32,
+    this.backgroundColor = backgroundPrimary,
   }) : super(key: key);
 
   final String? imageUrl;
   final String username;
   final bool isOnline;
+  final double? width;
+  final double? height;
+  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          width: 32,
-          height: 32,
+          width: width,
+          height: height,
           decoration: BoxDecoration(
-            color: backgroundPrimary,
-            borderRadius: BorderRadius.circular(16),
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(width! / 2),
           ),
           clipBehavior: Clip.antiAlias,
           child: imageUrl == null
               ? Center(child: Text(username.substring(0, 1)))
-              : Image.network(imageUrl!),
+              : Image.network(imageUrl!, fit: BoxFit.cover),
         ),
         if (isOnline)
           const Positioned(
